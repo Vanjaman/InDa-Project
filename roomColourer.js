27035deb4a56acb4ee7738a3d32864fe5a21add9
Vanja Grigoriev <ivang@kth.se>
2022-04-22 02:19:09 +0200

    Moved js script outside of SVG to it's own file.

---
+++ roomColourer.js
@@ -0,0 +1,20 @@
+// This script is run by an SVG file to colour it's rooms red.
+// The code that runs this file is found at the bottom of an SVG file.
+// Author: Vanja Grigoriev
+// Date: 2022/04/20
+var data = document.defaultView.location.href.split("?")[1];
+
+colourRooms(data);
+
+function colourRooms(text) {
+    var data = text.split(",");
+    for (var room=0; room<data.length; room++) {
+        colourRoom(data[room]);
+    }
+}
+
+function colourRoom(name) {
+    var country = document.getElementById(name);
+    var newClass = 'room red';
+    country.setAttributeNS(null, 'class', newClass);
+}
