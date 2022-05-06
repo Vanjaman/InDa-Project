55d077561e5ce9ed5cc7f79b7b943afffdebf64f
Vincent Lindvall <vinlin@kth.se>
2022-05-06 18:42:03 +0200

    Code for deciding what SVGs should be loaded
    The code uses the JSON house structure file that describes how floors are connected in different buildings at KTH. The code is used to decide what SVGs that should be loaded in a building view (the main SVG and the two maps in the preview boxes).

--- src/loadSVGs.js
+++ src/loadSVGs.js
@@ -15,21 +15,39 @@ var mainSVG;
 var floorUpPreview;
 var floorDownPreview;
 
+// JSON file containing information about how floors are connected in different buildings
+var floorStructuresFile;
+var floorStructuresText;
+var floorStructuresJSON;
+var houseObject;
+
 // Fetch the file that describes how the floors are connected in the different buildings at KTH
+// This file is run initially when the user enters a building view, and fetches the names of the
+// SVGs that should be loaded.
 async function fetchEntrySVGs() {
-    var floorStructuresFile = await fetch('houseStructures.json');
-    var floorStructuresText = await floorStructuresFile.text();
-    var floorStructuresJSON = JSON.parse(floorStructuresText);
+    floorStructuresFile = await fetch('houseStructures.json');
+    floorStructuresText = await floorStructuresFile.text();
+    floorStructuresJSON = JSON.parse(floorStructuresText);
 
     // D-E-house
     if (house == "D-E-house") {
-        var houseObject = floorStructuresJSON['D-E-house'];
+        houseObject = floorStructuresJSON['D-E-house'];
         mainSVG = houseObject['entry-floor'];
         floorDownPreview = houseObject['entry-down-floor'];
         floorUpPreview = houseObject['entry-up-floor'];
-
-        console.log(mainSVG, floorDownPreview, floorUpPreview)
     }
 
-    return [mainSVG, floorDownPreview, floorUpPreview]
+    return [mainSVG, floorUpPreview, floorDownPreview]
+}
+
+// This function is used to fetch the name of the given 'floorName' and the
+// two floors that the 'floorName' is connected to according to the house structures
+// JSON file. This file is used when the user switches floor in the building view.
+async function fetchSVGs(floorName) {
+    var floor = houseObject[floorName];
+    mainSVG = floor['floorName'];
+    floorDownPreview = floor['floorDown'];
+    floorUpPreview = floor['floorUp'];
+
+    return [mainSVG, floorUpPreview, floorDownPreview]
 }
