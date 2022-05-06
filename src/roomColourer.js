e4166de9ab843d76ba297418c70cc0420f6142a6
Vincent Lindvall <vinlin@kth.se>
2022-05-06 18:48:48 +0200

    Switch floors and show correct floors in preview
    It's now possible to switch floor by clicking on on e of the preview boxes in the building view. This takes you to the correct floor, updates and re-colours the different SVGs shown in the building view. The correct floors are now shown when the user enters the building view by reading from the JSON house structures file that contains information about this and how the different floors are connected

--- src/roomColourer.js
+++ src/roomColourer.js
@@ -1,25 +1,94 @@
-// This script colors booked premises red in an SVG.
-// Information about which premises are booked comes from a seperate
-// csv-reader file.
+/* This script colors booked premises red in the SVGs displayed in a building view.
+   Information about which premises are booked comes from a seperate
+   csv-reader file. The initial (entry-level of building) SVGs are loaded when this
+   script is called from enterying the building view. The SVGs are updated when an
+   up- or down-floor button is pressed, and rooms are re-colored.
+*/
 // Author: Vanja Grigoriev, Vincent Lindvall
-// Date: 2022/04/23
+// Date: 2022/05/05
 
-// The SVG to manipulate
-var svgObject = document.getElementById('svg-object');
-var SVGDocument;
-// Wait for the SVG to load
-svgObject.onload = function() {
-    // The contentDocument carries all the tags for the rooms in the SVG
-    SVGDocument = svgObject.contentDocument;
-    fetchRoomsToColor()
+// Class to be applied to booked premises in the SVG
+var bookedRoomClass = 'room booked';
+
+// List of (string) SVG names to display in format [mainSVG, floorUpSVG, floorDownSVG]
+var svgNames = [];
+
+// comma-seperated string with ID:s for premises to color
+var roomsToColor = [];
+
+// SVGs to display in the building view
+var svgMain = document.getElementById('svg-main-object');
+var svgFloorUp = document.getElementById('floor-up-preview');
+var svgFloorDown = document.getElementById('floor-down-preview');
+
+// Load the the Initial (entry-level of building) SVGs to manipulate
+// This function will only run once when entering the building view
+// to set up the SVGs that should be shown in the entry-level of the building
+async function loadInitialSVGsAndColor() {
+    svgNames = await fetchEntrySVGs().then(res => svgNames = res);
+
+    svgMain.setAttribute('data', 'maps/ED/'.concat(svgNames[0], ".svg"))
+    svgFloorUp.setAttribute('data', 'maps/ED/'.concat(svgNames[1], ".svg"))
+    svgFloorDown.setAttribute('data', 'maps/ED/'.concat(svgNames[2], ".svg"))
+
+    svgMain.onload = function() {
+        svgFloorUp.onload = function() {
+            svgFloorDown.onload = function() {
+                fetchRoomsToColor()
+            }
+        }
+    }
 }
+loadInitialSVGsAndColor()
 
+// Load what rooms should be colored. This will be run when loading the building view page,
+// and when the user selects a new time (modifies the rooms that should be colored)
 async function fetchRoomsToColor() {
-    var roomsToColor = await fetchAndParseCSVData();
+    roomsToColor = await fetchAndParseCSVData();
     roomsToColor = roomsToColor.split(", ");
-    colourRooms(roomsToColor);
+    colourRooms(roomsToColor)
 }
 
+// When the user has clicked on the 'floor up' or 'floor down' button in the building view,
+// the maps shown should be updated. 'svgMain' (string) is the name of the newly selected floor,
+// and (string) 'svgFloorup' and (string) 'svgFloorDown' are the names of the floors that the newly
+// selected floor is connected with, and are shown in the preview boxes in the building view
+// (previews will not be shown in a future mobile version).
+async function switchFloors(direction) {
+    // direction = 1 means go up, direction = 2 means go down
+    svgNames = await fetchSVGs(svgNames[direction]);
+
+    svgMain.onload = function() {
+        svgFloorUp.onload = function() {
+            svgFloorDown.onload = function() {
+                colourRooms(roomsToColor)
+            }
+        }
+    }
+
+    // Switch the SVGs
+    svgMain.setAttribute('data', 'maps/ED/'.concat(svgNames[0], ".svg"))
+    // If null, the SVG should be hidden and not try to load in an undefined SVG
+    // (will cause problems with the .onload functions)
+    if (svgNames[1] != null) {
+        svgFloorUp.style.display = "block";
+        svgFloorUp.setAttribute('data', 'maps/ED/'.concat(svgNames[1], ".svg"))
+    } else {
+        svgFloorUp.style.display = "none";
+    }
+    // If null, the SVG should be hidden and not try to load in an undefined SVG
+    // (will cause problems with the .onloa
+    if (svgNames[2] != null) {
+        svgFloorDown.style.display = "block";
+        svgFloorDown.setAttribute('data', 'maps/ED/'.concat(svgNames[2], ".svg"))
+    } else {
+        svgFloorDown.style.display = "none";
+    }
+}
+
+// Colours the given rooms. 'data' is an array of strings that contain the IDs of the 
+// premises that should be colored red. If an ID is not in the array, the default color
+// of a bookable premise is green.
 function colourRooms(data) {
     for (var room=0; room<data.length; room++) {
         colourRoom(data[room]);
@@ -27,9 +96,14 @@ function colourRooms(data) {
 }
 
 function colourRoom(name) {
-    var room = SVGDocument.getElementById(name);
-    var newClass = 'room booked';
-    if (room != null) {
-        room.setAttributeNS(null, 'class', newClass); 
-    }
+    var svgMainRoom = svgMain.contentDocument.getElementById(name);
+    var svgFloorUpRoom = svgFloorUp.contentDocument.getElementById(name);
+    var svgFloorDownRoom = svgFloorDown.contentDocument.getElementById(name);
+    var rooms = [svgMainRoom, svgFloorUpRoom, svgFloorDownRoom];
+    
+    rooms.forEach((room) => {
+        if (room != null) {
+            room.setAttributeNS(null, 'class', bookedRoomClass); 
+        }
+    })
 }
