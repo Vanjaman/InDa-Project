45a307bcfc779272b047af1ff99dc2bc930d2c58
Vincent Lindvall <vinlin@kth.se>
2022-05-07 20:23:12 +0200

    fixed problem related to parallellalism, added documentation
    The parallellalism problem regarded when waiting for the SVGs to load

--- src/roomColourer.js
+++ src/roomColourer.js
@@ -5,7 +5,7 @@
    up- or down-floor button is pressed, and rooms are re-colored.
 */
 // Author: Vanja Grigoriev, Vincent Lindvall
-// Date: 2022/05/06
+// Date: 2022/05/07
 
 
 // Get house name
@@ -18,10 +18,10 @@ if (house == "D-E-house") {
 // Class to be applied to booked premises in the SVG
 var bookedRoomClass = 'room booked';
 
-// List of (string) SVG names to display in format [mainSVG, floorUpSVG, floorDownSVG]
+// List of (string) SVG names to display (format [mainSVG, floorUpSVG, floorDownSVG])
 var svgNames = [];
 
-// comma-seperated string with ID:s for premises to color
+// string list with ID:s for premises to color
 var roomsToColor = [];
 
 // SVGs to display in the building view
@@ -38,21 +38,36 @@ async function loadInitialSVGsAndColor() {
     // Load correct floor title
     document.getElementById('floor-info').innerHTML = houseName.concat("- Floor ",svgNames[0].charAt(svgNames[0].length-1));
 
+    // Load SVGs
     svgMain.setAttribute('data', 'maps/ED/'.concat(svgNames[0], ".svg"))
     svgFloorUp.setAttribute('data', 'maps/ED/'.concat(svgNames[1], ".svg"))
     svgFloorDown.setAttribute('data', 'maps/ED/'.concat(svgNames[2], ".svg"))
 
-    svgMain.onload = function() {
-        svgFloorUp.onload = function() {
-            svgFloorDown.onload = function() {
+    // Wait for SVGs to load before coloring them
+    loadedSVGs = 0; // Will work because Javascript is single threaded
+    svgList = [svgMain, svgFloorUp, svgFloorDown];
+    for (var i = 0; i < svgList.length; i++) {
+        // If the SVG is not found consider it already loaded
+        if (svgNames[i] == null) {
+            loadedSVGs += 1;
+        }
+        // Add to loadedSVGs when loaded
+        svgList[i].onload = function() {
+            loadedSVGs += 1;
+            if (loadedSVGs == svgList.length) {
+                // Fetch the rooms that should be colored red (booked premises)
                 fetchRoomsToColor()
             }
+        }
+        if (loadedSVGs == svgList.length) {
+            // Fetch the rooms that should be colored red (booked premises)
+            fetchRoomsToColor()
         }
     }
 }
 loadInitialSVGsAndColor()
 
-// Load what rooms should be colored. This will be run when loading the building view page,
+// Load what rooms should be colored. This will run when loading the building view page,
 // and when the user selects a new time (modifies the rooms that should be colored)
 async function fetchRoomsToColor() {
     roomsToColor = await fetchAndParseCSVData();
@@ -61,7 +76,7 @@ async function fetchRoomsToColor() {
 }
 
 // When the user has clicked on the 'floor up' or 'floor down' button in the building view,
-// the maps shown should be updated. 'svgMain' (string) is the name of the newly selected floor,
+// the maps shown will be updated. (string) 'svgMain' is the name of the newly selected floor,
 // and (string) 'svgFloorup' and (string) 'svgFloorDown' are the names of the floors that the newly
 // selected floor is connected with, and are shown in the preview boxes in the building view
 // (previews will not be shown in a future mobile version).
@@ -69,12 +84,25 @@ async function switchFloors(direction) {
     // direction = 1 means go up, direction = 2 means go down
     svgNames = await fetchSVGs(svgNames[direction]);
 
-    svgMain.onload = function() {
-        svgFloorUp.onload = function() {
-            colourRooms(roomsToColor)
-            svgFloorDown.onload = function() {
-                colourRooms(roomsToColor)
+    // Wait for SVGs to load before coloring them
+    loadedSVGs = 0; // Will work because Javascript is single threaded
+    svgList = [svgMain, svgFloorUp, svgFloorDown];
+    for (var i = 0; i < svgList.length; i++) {
+        // If the SVG is not found consider it already loaded
+        if (svgNames[i] == null) {
+            loadedSVGs += 1;
+        }
+        // Add to loadedSVGs when loaded
+        svgList[i].onload = function() {
+            loadedSVGs += 1;
+            if (loadedSVGs == svgList.length) {
+                // Fetch the rooms that should be colored red (booked premises)
+                colourRooms(roomsToColor);
             }
+        }
+        if (loadedSVGs == svgList.length) {
+            // Fetch the rooms that should be colored red (booked premises)
+            colourRooms(roomsToColor);
         }
     }
 
@@ -86,22 +114,22 @@ async function switchFloors(direction) {
     // If null, the SVG should be hidden and not try to load in an undefined SVG
     // (will cause problems with the .onload functions)
     if (svgNames[1] != null) {
-        svgFloorUp.style.display = "block";
+        svgFloorUp.style.display = "block"; // Make visible
         svgFloorUp.setAttribute('data', 'maps/ED/'.concat(svgNames[1], ".svg"))
     } else {
-        svgFloorUp.style.display = "none";
+        svgFloorUp.style.display = "none"; 
     }
     // If null, the SVG should be hidden and not try to load in an undefined SVG
-    // (will cause problems with the .onloa
+    // (will cause problems with the .onload functions)
     if (svgNames[2] != null) {
-        svgFloorDown.style.display = "block";
+        svgFloorDown.style.display = "block"; // Make visible
         svgFloorDown.setAttribute('data', 'maps/ED/'.concat(svgNames[2], ".svg"))
     } else {
         svgFloorDown.style.display = "none";
     }
 }
 
-// Colours the given rooms. 'data' is an array of strings that contain the IDs of the 
+// Colours the given SVG. 'data' is an array of strings that contain the IDs of the 
 // premises that should be colored red. If an ID is not in the array, the default color
 // of a bookable premise is green.
 function colourRooms(data) {
@@ -110,6 +138,8 @@ function colourRooms(data) {
     }
 }
 
+// Colours the premise corresponding to the given premise ID if present in any
+// of the loaded SVGs in the building view.
 function colourRoom(name) {
     // getElementById will return  null if not found on that floor
     var svgMainRoom = svgMain.contentDocument.getElementById(name);
