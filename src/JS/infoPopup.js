dc02c7cddb1601f76def0f40c24fb96f29ec4def
Vincent Lindvall <vinlin@kth.se>
2022-05-18 18:13:16 +0200

    The popup works as expected now
    Works as expected now according to issue #39

--- src/JS/infoPopup.js
+++ src/JS/infoPopup.js
@@ -1,3 +1,7 @@
+// This code is used to display an informative popup when the user hovers over a premise in the building view
+//
+// Author: Vincent Lindvall
+// Date: 2022-05-18
 
 function setUpInfoWhenHover() {
     // Only rooms that are booked or bookable should have their information shown when
@@ -13,12 +17,41 @@ function setUpInfoWhenHover() {
     for (var typeOfRoom = 0; typeOfRoom < 2; typeOfRoom++) { // type of room is "booked" or "bookable"
         roomsToDisplayInfoAbout[typeOfRoom].forEach(room => {
             if (room != null) {
-                room.addEventListener('mouseover', displayInfo)
+                room.addEventListener('mousemove', displayInfo)
+                room.addEventListener('mouseout', removeDisplayInfo)
             }
         });
     }
 
-    function displayInfo() {
-        console.log(Object.keys(premiseNameToIDMap).find(key => premiseNameToIDMap[key] === this.id))
+    // When the user hovers over a premise, show information popup about that premise
+    function displayInfo(e) {
+        // Position information in pixels on SVG
+        var x = e.pageX;
+        var y = e.pageY;
+        var svgRect = svgMain.getBoundingClientRect(); // Rectangle that holds the SVG
+
+        // Highlight the premise
+        this.style.stroke = 'black';
+        this.style.strokeWidth = '1px';
+        this.style.cursor = "pointer";
+
+        // Display the info-popup
+        var infoPopup = document.getElementById('info-popup');
+        infoPopup.style.left = x+svgRect.x+"px";
+        infoPopup.style.top = y+svgRect.y*0.7+"px"; // *0.7 to make the pop-up appear above the cursor
+        infoPopup.style.visibility = "visible";
+        var premiseName = Object.keys(premiseNameToIDMap).find(key => premiseNameToIDMap[key] === this.id);
+        infoPopup.innerHTML = premiseName;
+    }
+
+    // When the user no longer hovers a premise, remove the popup with info about the premise
+    function removeDisplayInfo() {
+        // De-highlight the premise
+        this.style.strokeWidth = '0.1px';
+        this.style.cursor = "pointer";
+
+        // Hide the popup
+        var infoPopup = document.getElementById('info-popup');
+        infoPopup.style.visibility = "hidden";
     }
 }
